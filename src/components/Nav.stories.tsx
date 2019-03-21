26a79d8c6e8d0ab129eee588d8ae6fe506174422
kosak11 <bartoszkozak94@gmail.com>
2019-03-21 15:27:39 +0100

    Added fixs and Ripple Effect in Nav

--- src/components/Nav.stories.tsx
+++ src/components/Nav.stories.tsx
@@ -11,15 +11,17 @@ const options = {
     Yellow: 'yellow',
     Rainbow: ['red', 'orange', 'etc'],
     None: null,
-  };
+}
 
 storiesOf('Nav', module)
     .addDecorator(withKnobs)
-    .add('test', () => <div>
-        Test1: {text('Word', 'Hello')} <br/>
-        Test2: {number('Dupa number', 0)}
-        Test4: {select('Color', options, 'red')}
-    </div>)
+    .add('test', () => (
+        <div>
+            Test1: {text('Word', 'Hello')} <br />
+            Test2: {number('Dupa number', 0)}
+            Test4: {select('Color', options, 'red')}
+        </div>
+    ))
     .add('state test', () => <NavStated />)
 
 export const NavStated = () => {
@@ -30,23 +32,10 @@ export const NavStated = () => {
         </NavPosition>
     )
 }
-    
-    // teraz pytanie :
-    // nie działa klik ta sie go zrobić na szybko?
-    //  to "klik KURWA" chce zamienić na to żeby zmieniał mi na acita ale // on ma nie zmieniać
-    // da sie?
-    // czy powinnem to poporstu testować za pomoca tych parametrów po prawej
-    // mozesz sobie napisać swój mały komponent ze stanem i go wyrenderować ale nie o to tu chodzi 
-    //  to bez sensu chyba troche
-    // no to juz będzie w appce
-    //  no w apce działa ale ogarniam o co chodzi
-    // podoba mi sie to że możesz sobie pisać swój komponent i dotykając reszyty apki
-    const NavPosition = styled.div`
+
+const NavPosition = styled.div`
     position: fixed;
     bottom: 0;
     right: 0;
     left: 0;
 `
-
-
-
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
+import { white, gray, brand } from '../styles/colors'
+import { barShadow } from '../styles/shadow'
 
 type NavProps = {
     active: string
@@ -9,65 +11,59 @@ type NavProps = {
 export const Nav = (prop: NavProps) => {
     return (
         <Container>
-                <NavButton isActive={prop.active==='home'} onClick={() => prop.onChange('home')}>Home</NavButton>
-                <NavButton isActive={prop.active==='words'} onClick={() => prop.onChange('words')}>Words</NavButton>
-                <NavButton isActive={prop.active==='settings'} onClick={() => prop.onChange('settings')}>
-                    Settings
-                </NavButton>
+            <NavButton
+                isActive={prop.active === 'home'}
+                onClick={() => prop.onChange('home')}
+            >
+                Home
+            </NavButton>
+            <NavButton
+                isActive={prop.active === 'words'}
+                onClick={() => prop.onChange('words')}
+            >
+                Words
+            </NavButton>
+            <NavButton
+                isActive={prop.active === 'settings'}
+                onClick={() => prop.onChange('settings')}
+            >
+                Settings
+            </NavButton>
         </Container>
     )
 }
-// jest sens używać takich elemntów jak ????????? 
-// bo trzeba im zerować np paddingi
-// Git, dlaczego nie?
 const Container = styled.div`
-    background: white;
+    background: ${white};
     display: flex;
     width: 100%;
+    min-height: 60px;
     justify-content: space-evenly;
+    box-shadow: ${barShadow};
+    overflow: hidden;
 `
-
-// takie rzczy jak position fixed to sie robi tu czy w app?
-// do całego componetu
-// Generlanie dobrze jak komponenty nie wychodzą po za swój kontener
-// czyli bez marginesów, positionów itd.
-// Bo pewnie w app będzie jakiś layout i wtedy wsadziesz sobie nav na dole
-// Ale czasami łamie się te zasady, zalezy jak Ci wygodnie
-// Np. ja klawiatury nie chciałem robić absolute bo moze przy ustawieniach ja gdzieś indziej wyswietę jako podglad 
-// ok czaje
-// można stylować takie rzeczy jak <Nav/>?
-//  →→→ A co chcesz w tym stylować?
-// no NAV w pliku nav.stories.tsx
-// zeby ja testować na dole np xD
-// No ja zrobiłem sobie diva który mi przeniusł to na 
-//  Nie tykam stylów klawiatury, klawiatura ma sowje propsy i tyle
-// więc jak chce mieć duza nawigację to robie <Nav isBig />
-
-// no czaje choc do nav stories!
-
-
-// za dużo nas w jednym miejsc xD
 
 const NavButton = styled.button<{ isActive: boolean }>`
-    background: ${props => props.isActive ? 'red' : 'black'};
+    color: ${props => (props.isActive ? brand : gray)};
     display: block;
+    background: none;
+    border: none;
+    padding: 10px 25px;
+    position: relative;
+    &::after {
+        content: '';
+        display: block;
+        border-radius: 100%;
+        height: 60px;
+        width: 60px;
+        position: absolute;
+        margin: auto;
+        top: -50px;
+        bottom: -50px;
+        left: -50px;
+        right: -50px;
+        transition: all ${props => (props.isActive ? 0.3 : 0)}s ease;
+        opacity: ${props => (props.isActive ? 0 : 0.3)};
+        transform: scale(${props => (props.isActive ? 2 : 1)});
+        background: ${props => (props.isActive ? brand : 'rgba(255,255,255,0)')};
+    }
 `
-//////////////
-// to kawałek TS (nie react i nie styled components) i typów generycznych
-// masz np typ Promise ale promise moze zwracać  więć się pisze
-// Promise<string> albo Promise<boolean> albo Promise<{ userName: string, isActive: false }> itd.
-// Ten typ "generyczny" mozna podać tez przy funkcji jak wykonujesz np. <- loading...
-// fetch<{ emalWasSend: boolean }>('api/send-email') <- loading...
-
-
-// ładniej, ctrl+z się szarpie
-// ja jestem pod wrażeniem że to i tak, tak dobrze działa
-
-// nie ogarniam tego TS troche
-
-// to znaczy że obiekt ApiResponse ma parametr emalWasSend który jest boolean?
-// type ApiResponse = { emalWasSend: boolean }
-
-// a tego nie rozumiem
-// fetch<ApiResponse>('api/send-email')}
-// aaa chyba czaje to znaczy że cos zwraca wedlug tego klucza co ApiResponse czy jako tka
