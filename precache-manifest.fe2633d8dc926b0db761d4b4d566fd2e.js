259052fd98a76f63670f9d190b7e1d849fe12eb7
Kacper Kozak <kacperkk@gmail.com>
2019-03-26 16:51:28 +0100

    Updates

---
+++ precache-manifest.fe2633d8dc926b0db761d4b4d566fd2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ba777dd01676db9263e",
+    "url": "/static/js/main.6bf0c38f.chunk.js"
+  },
+  {
+    "revision": "f1047c22da5165e097b6",
+    "url": "/static/js/2.07a699b4.chunk.js"
+  },
+  {
+    "revision": "2ba777dd01676db9263e",
+    "url": "/static/css/main.e11254c5.chunk.css"
+  },
+  {
+    "revision": "2c9833a24efa233e8309a1c57fb166c6",
+    "url": "/index.html"
+  }
+];
---
+++ storybook/precache-manifest.c71445deed28ac364f5275ccf5876973.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.d5fb6d5f0085fdc4cf0c.bundle.js"
+  },
+  {
+    "url": "runtime~main.d5fb6d5f0085fdc4cf0c.bundle.js"
+  },
+  {
+    "url": "main.d5fb6d5f0085fdc4cf0c.bundle.js"
+  },
+  {
+    "revision": "929da0632a6a455ba762",
+    "url": "main.css"
+  },
+  {
+    "revision": "91d34e4b5e36d88355161fddf7d60506",
+    "url": "iframe.html"
+  }
+];
