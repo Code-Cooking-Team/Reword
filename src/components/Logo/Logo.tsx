a068a62d5805115bea2fae2b8d475ba35874388f
Kacper Kozak <kacperkk@gmail.com>
2019-03-28 18:03:22 +0100

    New logo animation

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,39 +4,59 @@ import { useSpring, animated } from 'react-spring'
 import { brand } from '../../styles/colors'
 
 export const Logo = () => {
-    const reSpring = useSpring({ opacity: 1, delay: 200, from: { opacity: 0 } })
-    const ordSpring = useSpring({ opacity: 1, delay: 400, from: { opacity: 0 } })
-    const sepSpring = useSpring({
+    const reSpring = useSpring({
+        opacity: 1,
         transform: 'translate(0)',
-        from: { transform: 'translate(-1.2em)' },
+        delay: 200,
+        from: {
+            opacity: 0,
+            transform: 'translate(-0.2em)',
+        },
+    })
+    const wordSpring = useSpring({
+        opacity: 1,
+        transform: 'translate(0)',
+        delay: 300,
+        from: {
+            opacity: 0,
+            transform: 'translate(-0.2em)',
+        },
+    })
+    const sepSpring = useSpring({
+        transform: 'translate(0) scaleX(1)',
+        from: {
+            transform: 'translate(-1.2em) scaleX(5)',
+        },
     })
 
     return (
         <Container>
             <First style={reSpring}>Re</First>
-            <Sep style={sepSpring}>|</Sep>
-            <Last style={ordSpring}>word</Last>
+            <Sep style={sepSpring} />
+            <Last style={wordSpring}>word</Last>
         </Container>
     )
 }
 
 const Container = styled.h1`
-    display: flex;
-    justify-content: center;
+    text-align: center;
     font-size: 74px;
     font-weight: normal;
 `
 
 const First = styled(animated.span)`
-    display: block;
+    display: inline-block;
 `
 
 const Last = styled(animated.span)`
-    display: block;
+    display: inline-block;
 `
 
 const Sep = styled(animated.span)`
-    display: block;
-    font-weight: bold;
-    color: ${brand};
+    display: inline-block;
+    height: 1em;
+    width: 0.1em;
+    margin: 0 0.05em;
+    vertical-align: middle;
+    background-color: ${brand};
 `
