bb4748b56313c5b3b82c1c17dfe45018680d062b
Kacper Kozak <kacperkk@gmail.com>
2019-06-17 12:06:51 +0200

    Fix initial route

--- src/store/state/initialState.ts
+++ src/store/state/initialState.ts
@@ -2,7 +2,7 @@ import { RouteName } from '../types/RouteName'
 import { State } from './types/State'
 
 export const initialState: State = {
-    route: RouteName.Words,
+    route: RouteName.Home,
     lastRoutes: [],
     words: undefined,
     user: undefined,
