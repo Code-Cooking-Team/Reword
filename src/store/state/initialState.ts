64afd2632ffd0a2047934d7bd33d7b18d77d4dd7
Kacper Kozak <kacperkk@gmail.com>
2020-03-01 14:50:07 +0100

    Fix initial route

--- src/store/state/initialState.ts
+++ src/store/state/initialState.ts
@@ -2,7 +2,7 @@ import { RouteName } from '../types/RouteName'
 import { State } from './types/State'
 
 export const initialState: State = {
-    route: RouteName.Game, // TODO
+    route: RouteName.Home,
     lastRoutes: [],
     words: undefined,
     user: undefined,
